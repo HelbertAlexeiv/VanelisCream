a13fc5529b7b29000b59bf49b098d5125449a1da
HelbertAlexeiv <helbertalexeivcorreauribe@gmail.com>
2026-03-11 11:38:12 -0500

    feat: crear modelos para pedido

--- backend/pedidos/admin.py
+++ backend/pedidos/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import AuditoriaPedido, DetallePedido, EstadoPedido, Pedido
 # Register your models here.
+admin.site.register(EstadoPedido)
+admin.site.register(Pedido)
+admin.site.register(DetallePedido)
+admin.site.register(AuditoriaPedido)
--- backend/pedidos/models.py
+++ backend/pedidos/models.py
@@ -1,3 +1,60 @@
 from django.db import models
+from django.db.models import Sum, F
+from django.conf import settings
 
-# Create your models here.
+class EstadoPedido(models.Model):
+
+    nombre = models.CharField(max_length=50, unique=True)
+    descripcion = models.TextField(blank=True, null=True)
+
+    class Meta:
+        db_table = "estado_pedido"
+
+    def __str__(self):
+        return self.nombre
+    
+class Pedido(models.Model):
+
+    cliente = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.PROTECT, related_name="pedidos_cliente")
+    empleado = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.PROTECT, related_name="pedidos_empleado", blank=True, null=True)
+    estado = models.ForeignKey(EstadoPedido, on_delete=models.PROTECT, related_name="pedidos")
+    fecha_creacion = models.DateTimeField(auto_now_add=True)
+    direccion_entrega = models.CharField(max_length=200)
+    total_pedido = models.DecimalField(max_digits=10, decimal_places=2)
+
+    class Meta:
+        db_table = "pedido"
+
+    def __str__(self):
+        return f"Pedido {self.id}"
+    
+    def actualizar_total(self):
+        total = self.detalles.aggregate(
+            total=Sum(F("producto__precio") * F("cantidad"))
+        )["total"] or 0
+
+        self.total_pedido = total
+        self.save()
+
+class DetallePedido(models.Model):
+
+    pedido = models.ForeignKey(Pedido, on_delete=models.CASCADE, related_name="detalles")
+    producto = models.ForeignKey('catalogo.Producto', on_delete=models.PROTECT, related_name="detalles_pedido")
+    cantidad = models.PositiveIntegerField()
+
+    class Meta:
+        db_table = "detalle_pedido"
+
+    def __str__(self):
+        return f"{self.producto.nombre} x {self.cantidad}"
+    
+
+class AuditoriaPedido(models.Model):
+
+    pedido = models.ForeignKey(Pedido, on_delete=models.SET_NULL, null=True)
+    usuario = models.CharField(max_length=100)
+    accion = models.CharField(max_length=20)
+    campo_modificado = models.CharField(max_length=50)
+    valor_anterior = models.CharField(max_length=255, null=True, blank=True)
+    valor_nuevo = models.CharField(max_length=255, null=True, blank=True)
+    fecha_evento = models.DateTimeField(auto_now_add=True)
