86a8bc580fe0e32bf088b5af8378f33b795180d6
HelbertAlexeiv <helbertalexeivcorreauribe@gmail.com>
2026-03-11 10:34:42 -0500

    feat: crear modelos usuarios

--- backend/usuarios/models.py
+++ backend/usuarios/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class Rol(models.Model):
+    nombre = models.CharField(max_length=100, unique=True)
+
+    class Meta:
+        db_table = "rol"
+        verbose_name = "Rol"
+        verbose_name_plural = "Roles"
+
+    def __str__(self):
+        return self.nombre
+    
+class Usuario(AbstractUser):
+
+    rol = models.ForeignKey(Rol, on_delete=models.PROTECT, related_name="usuarios")
+    telefono = models.CharField(max_length=20, blank=True, null=True)
+    direccion = models.CharField(max_length=100, blank=True, null=True)
+
+    class Meta:
+        db_table = "usuario"
+
+    def __str__(self):
+        return self.username
+
