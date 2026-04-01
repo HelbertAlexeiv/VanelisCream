7d15292091eac0ba4e39a6017de0c0cc4b8998f4
HelbertAlexeiv <helbertalexeivcorreauribe@gmail.com>
2026-04-01 06:44:18 -0500

    feat(pedidos): agregar autorizacion

--- backend/pedidos/urls.py
+++ backend/pedidos/urls.py
@@ -3,6 +3,6 @@ from rest_framework.routers import DefaultRouter
 from .views import PedidoViewSet 
 
 router = DefaultRouter()
-router.register(r'pedidos', PedidoViewSet, basename='pedido')
+router.register(r'', PedidoViewSet, basename='pedido')
 
 urlpatterns = router.urls
--- backend/pedidos/views.py
+++ backend/pedidos/views.py
@@ -4,9 +4,15 @@ from django.shortcuts import render
 from rest_framework import viewsets
 from rest_framework.decorators import action
 from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+
+from .models import Pedido
+from .serializers import PedidoCreateSerializer, PedidoSerializer
 
 class PedidoViewSet(viewsets.ModelViewSet):
     queryset = Pedido.objects.all()
+
+    permission_classes = [IsAuthenticated]
 
     def get_serializer_class(self):
         if self.action == 'create':
