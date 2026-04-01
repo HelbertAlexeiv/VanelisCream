94f332ff7da6b6cf406d9f69be838fd9961a0321
HelbertAlexeiv <helbertalexeivcorreauribe@gmail.com>
2026-03-31 20:47:26 -0500

    feat: crear endpoints pedido

--- backend/pedidos/views.py
+++ backend/pedidos/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework import viewsets
+from rest_framework.decorators import action
+from rest_framework.response import Response
+
+class PedidoViewSet(viewsets.ModelViewSet):
+    queryset = Pedido.objects.all()
+
+    def get_serializer_class(self):
+        if self.action == 'create':
+            return PedidoCreateSerializer
+        return PedidoSerializer
+
+    def get_queryset(self):
+        user = self.request.user
+        if user.is_staff:
+            return Pedido.objects.all()
+        return Pedido.objects.filter(cliente=user)
